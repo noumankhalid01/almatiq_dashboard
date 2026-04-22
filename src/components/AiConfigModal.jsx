01cd13486a4420cfa34640498cca2d5507206005
Nouman Khalid <noumankhalid873@gmail.com>
2026-04-22 06:07:56 +0500

    kaira form btn text updated to confirm

--- src/components/AiConfigModal.jsx
+++ src/components/AiConfigModal.jsx
@@ -178,7 +178,7 @@ const AiConfigModal = ({ open, onComplete, onClose, initialValues = {} }) => {
                 Saving
               </span>
             ) : (
-              'Continue'
+              'Confirm'
             )}
           </button>
         </form>
